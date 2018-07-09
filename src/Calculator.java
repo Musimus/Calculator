d055f99b388624ea0bae015416e63e6217b4d529
Max Wilkinson <musimus4213@gmail.com>
2018-07-09 00:06:25 -0700

    Add commandline scanner

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,11 +1,28 @@
-
+import java.util.Scanner;
 public class Calculator 
 {
+	String userInput = null;
 
-	public String calculate(String userInput)
+	public String calculate(String tempNumToDelete)
 	{
+		userInput = askForUserInput();
 		
+		
+		
+		
+		return askForUserInput();
+		
+	}
+	
+	
+	public String askForUserInput()
+	{
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("What would you like to calculate?");
+		userInput = scanner.nextLine();
+		scanner.close();
 		return userInput;
 	}
+	
 
 }
--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		Calculator calc = new Calculator();
 		
-		System.out.println(calc.calculate("4+ 5"));
+		System.out.println(calc.calculate("test"));
 	}
 
 }
